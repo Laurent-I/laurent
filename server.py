39858ecc3df8e4d55f5805fe1f5aed8aaa7cd520
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-08 03:42:44 +0200

    Flask Templates

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+# @app.route('/<username>/<int:post_id>')
+# def hello_world(username=None, post_id=None):
+#     return render_template('index.html', name=username, post_id=post_id)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+# @app.route('/favicon.ico')
+# def about():
+#     return render_template('about.html')
