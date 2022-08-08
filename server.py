1a7a203c53a5f4945d510a836a8e163bfbdc98e9
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-08 03:43:59 +0200

    MIME Types

--- server.py
+++ server.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# @app.route('/<username>/<int:post_id>')
-# def hello_world(username=None, post_id=None):
-#     return render_template('index.html', name=username, post_id=post_id)
+@app.route('/<username>/<int:post_id>')
+def hello_world(username=None, post_id=None):
+    return render_template('index.html', name=username, post_id=post_id)
 
 
 @app.route('/about')
