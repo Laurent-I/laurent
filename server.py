0e136c946bd111dd35710650b31b97606831cc58
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-09 05:32:20 +0200

    Writing submitted data to a csv database

--- server.py
+++ server.py
@@ -41,13 +41,13 @@ def write_to_file(data):
         file = database.write(f'\n{email}, {subject}, {message}')
 
 
-# def write_to_csv(data):
-#     with open('database.csv', mode='a', newline='') as database2:
-#         email = data['email']
-#         subject = data['subject']
-#         message = data['message']
-#         csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#         csv_writer.writerow([email, subject, message])
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
 
 
 # @app.route('/submit', methods=['GET', 'POST'])
