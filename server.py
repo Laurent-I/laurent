9d67a1c1f5d8aee3737302c8438527f7ba4a9c88
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-09 05:29:14 +0200

    Dynamically rendering the routes

--- server.py
+++ server.py
@@ -9,9 +9,9 @@ def hello_world():
     return render_template('index.html')
 
 
-# @app.route('/<string:page_name>')
-# def html_page(page_name):
-#     return render_template(page_name)
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
 
 # @app.route('/about.html')
